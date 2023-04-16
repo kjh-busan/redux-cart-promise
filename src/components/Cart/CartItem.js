c9edbb0c81745c0beeb8ddf2130a47825022a7ea
kjh-busan <picsmash7@gmail.com>
2023-04-16 21:32:18 +0900

    fix: bug completed

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -6,12 +6,12 @@ import { cartActions } from "../../store/cart-slice";
 const CartItem = (props) => {
   const dispatch = useDispatch();
   const { title, quantity, total, price, id } = props.item;
-  console.log(id);
-  console.log(props);
-  console.log(title);
-  console.log(quantity);
-  console.log(total);
-  console.log(price);
+  // console.log(id);
+  // console.log(props);
+  // console.log(title);
+  // console.log(quantity);
+  // console.log(total);
+  // console.log(price);
 
   const removeItemHandler = (item) => {
     dispatch(cartActions.removeItemFromCart(id));
@@ -26,9 +26,7 @@ const CartItem = (props) => {
         <h3>{title}</h3>
         <div className={classes.price}>
           ${total.toFixed(2)}{" "}
-          <span className={classes.itemprice}>
-            (${price.toFixed(2) / price})
-          </span>
+          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
       <div className={classes.details}>
