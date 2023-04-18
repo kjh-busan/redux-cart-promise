bf35ff5fdf3f1f50682ad3284495e948fd3ca7b9
kjh-busan <picsmash7@gmail.com>
2023-04-18 23:26:25 +0900

    feat: useEffect with redux step1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,8 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {}, []);
 
   return (
     <Layout>
