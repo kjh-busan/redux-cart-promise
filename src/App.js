3e4c563925a17024cbf7437d74b44c46c20d3b1f
kjh-busan <picsmash7@gmail.com>
2023-04-20 23:09:24 +0900

    feat: notification

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
-  
+
   useEffect(() => {
-    fetch(
-      "https://mysecondproject-1d7a0-default-rtdb.asia-southeast1.firebasedatabase.app/",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        uiActions.showNotification({
+          status: 'pendings', title: 'sending', message: 'sending cart data!',
+        })
+    }))
+      const response = await fetch(
+        "https://mysecondproject-1d7a0-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        dispatch(
+          uiActions.showNotification({
+            status: 'error',
+            title: 'Error',
+            message: "Sending cart data failed"
+          })
+        )
       }
-    );
+
+      const responseData = await response.json();
+    };
   }, [cart]);
-  
+
   return (
+    <>
     <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
+    </>
   );
 }
 
