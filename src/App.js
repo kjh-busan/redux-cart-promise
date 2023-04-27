4e39406d3f17b295f415cdad9d6b832963e906d7
kjh-busan <picsmash7@gmail.com>
2023-04-27 22:55:15 +0900

    fix: error edit W.I.P

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { useEffect , useState } from "react";
+import { Fragement, useEffect , useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -25,23 +27,24 @@ function App() {
         }
       );
 
-      if (!response.ok) {
-        throw new Error("sending cart data failed.")
-      }
-
+      // if (!response.ok) {
+      //   throw new Error("sending cart data failed.")
+      // }
       // const responseData = await response.json();
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error',
-          message: "Sending cart data failed"
-        })
-      )
-  }, [cart]);
+      sendCartData().catch(error => {
+        dispatch(
+          uiActions.showNotification({
+            status: 'error',
+            title: 'Error',
+            message: "Sending cart data failed"
+          })
+        )
+      })
+  }, [cart, dispatch]);
 
   return (
     <>
+    {notification && (<Notification status={notification.status} title={notification.title} message={notification.message}/>)}
     <Layout>
       {showCart && <Cart />}
       <Products />
