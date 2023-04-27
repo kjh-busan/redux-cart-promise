a9ede56e263efbcd4e2d04ff5a4886a9accb814a
kjh-busan <picsmash7@gmail.com>
2023-04-27 22:40:43 +0900

    fix: error:

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { uiActions } from "./store/ui-slice";
-import NOtification from "./components/UI/
 
 function App() {
   const dispatch = useDispatch();
