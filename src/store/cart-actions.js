e3317e06be75ec3b160687f294d5d599a5b063b5
kjh-busan <picsmash7@gmail.com>
2023-04-30 18:15:48 +0900

    feat: add promise async await

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,5 +1,35 @@
 import { uiActions } from "./ui-slice";
 
+export const fetchCartData = () => {
+  return (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://mysecondproject-1d7a0-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      fetchData();
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
+
 export const sendCartData = (cart) => {
   return async (dispatch) => {
     dispatch(
